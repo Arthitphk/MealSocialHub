26a2961a539f74aab4330aa4ef4f8362333f376b
Arthitphk <128705950+Arthitphk@users.noreply.github.com>
2024-05-05 12:42:55 +0700

    add btn

--- foodhub/src/app/page.js
+++ foodhub/src/app/page.js
@@ -4,10 +4,28 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/feed">Feed Pages Here!</Link>
-      <Link href="/profile">Profile Pages Here!</Link>
-      <Link href="/chat">Chat Pages Here!</Link>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-800">
+      <button class="cursor-pointer inline-flex items-center rounded-full px-9 py-3 text-xl font-mono font-semibold text-rose-600 hover:text-white border-2 border-rose-600
+        hover:bg-rose-600 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-75  duration-300  focus:bg-transparent">
+        <Link href="/feed">Feed Pages Here!</Link>
+      </button>
+
+      <button class="cursor-pointer inline-flex items-center rounded-full px-9 py-3 text-xl font-mono font-semibold text-rose-600 hover:text-white border-2 border-rose-600
+        hover:bg-rose-600 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-75  duration-300  focus:bg-transparent">
+        <Link href="/profile">Profile Pages Here!</Link>
+      </button>
+
+      <button class="cursor-pointer inline-flex items-center rounded-full px-9 py-3 text-xl font-mono font-semibold text-rose-600 hover:text-white border-2 border-rose-600
+        hover:bg-rose-600 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-75  duration-300  focus:bg-transparent">
+        <Link href="/chat">Chat Pages Here!</Link>
+      </button>
     </main>
   );
 }
+
+
+
+
+
+
+
